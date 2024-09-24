1cda4ec8503a5b9252161604a01133df93eb6cfe
zhazgul00 <jazgul09.03@gmail.com>
2024-09-24 11:20:04 +0600

    added Individual Blog Pages

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,6 @@ class Post(models.Model):
         return self.title 
     
     def get_absolute_url(self):
-        return reverse("model_detail", kwargs={"pk": self.pk})
+        return reverse("post_detail", kwargs={"pk": self.pk})
     
     
