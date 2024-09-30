7bd24634c7eb6474007ddf100596c78c61549c97
zhazgul00 <jazgul09.03@gmail.com>
2024-09-30 12:13:11 +0600

    added delete

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = Post
@@ -15,7 +16,13 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
+
 class BlogUpdateView(UpdateView): 
     model = Post
     template_name = "post_edit.html"
     fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView): 
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
