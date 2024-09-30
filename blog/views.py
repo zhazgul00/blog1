6f242e7043c33f789f63f58d719b92faf3ce6ec1
zhazgul00 <jazgul09.03@gmail.com>
2024-09-30 12:06:44 +0600

    added blog edit and new

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.views.generic.edit import CreateView, UpdateView
 
 class BlogListView(ListView):
     model = Post
@@ -9,3 +10,12 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+class BlogCreateView(CreateView): 
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+class BlogUpdateView(UpdateView): 
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
