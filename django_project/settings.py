734edb881d4d4a18742f56d81d8fb4647b19f46d
zhazgul00 <jazgul09.03@gmail.com>
2024-10-21 12:34:05 +0600

    logout

--- django_project/settings.py
+++ django_project/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"] 
 LOGIN_REDIRECTR_URL = "home" 
+LOGOUT_REDIRECT_URL = "home" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
