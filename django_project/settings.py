1e0eca9a759146cacd19c84f74f14c94d2b75c7b
zhazgul00 <jazgul09.03@gmail.com>
2024-10-27 21:53:28 +0600

    Static Files

--- django_project/settings.py
+++ django_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic", 
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     "accounts.apps.AccountsConfig", 
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"] 
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATICFILES_STORAGE ="whitenoise.storage.CompressedManifestStaticFilesStorage"  
 LOGIN_REDIRECTR_URL = "home" 
 LOGOUT_REDIRECT_URL = "home" 
 
